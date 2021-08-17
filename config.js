178aa61d2230de70f86002ecad0af1429f765f29
FadliDarmawan <79369935+FadliDarmawan@users.noreply.github.com>
2021-08-17 20:42:13 +0700

    Update config.js
    into haruno's config

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 let fs = require('fs')
-global.owner = ['6283128734012'] // Letakan nomor kamu disini
+global.owner = ['628112958665','628998512588','6283175998566'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // nama: 'https://website'
-  bx: 'https://bx-hunter.herokuapp.com',
   hardianto: 'https://hardianto-chan.herokuapp.com',
   jonaz: 'https://jonaz-api-v2.herokuapp.com',
   neoxr: 'https://neoxr-api.herokuapp.com',
@@ -14,23 +13,31 @@ global.APIs = { // API Prefix
   zahir: 'https://zahirr-web.herokuapp.com',
   zekais: 'http://zekais-api.herokuapp.com',
   zeks: 'https://api.zeks.xyz',
+  vhtear: 'https://api.vhtear.com',
+  lolhum: 'https://api.lolhuman.xyz',
+  fxc7: 'https://fxc7-api.herokuapp.com',
+  aqul: 'https://justaqul.xyz'
+
 }
 global.APIKeys = { // APIKey nya disini
   // 'https://website': 'apikey'
-  'https://bx-hunter.herokuapp.com': 'Ikyy69',
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
-  'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
+  'https://pencarikode.xyz': 'APIKEY',
+  'https://api.xteam.xyz': 'Syahrulidhamz30',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.xyz': 'apivinz',
+  'https://api.zeks.xyz': 'FVe0JFCdME58YSKmhoDWdRtyuJt',
+  'https://api.lolhuman.xyz': 'BandarBokep',
+  'https://api.vhtear.com': 'sayahafiz',
+  'https://fxc7-api.herokuapp.com': 'uN8rsK4g',
+  'https://justaqul.xyz': '5kbUqdG00OXelFYx'
 }
 
 // Sticker WM
-global.packname = '❤️'
-global.author = 'ariffb'
+global.packname = 'Haruno'
+global.author = 'Fadli'
 
-global.wait = '_*tunggu..*_'
+global.wait = '「 ⏱️ 」Harap tunggu...'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 
