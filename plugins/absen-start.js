ed813ecea4f7145c9be6a1a017cff906ff6c21da
FadliDarmawan <79369935+FadliDarmawan@users.noreply.github.com>
2021-08-19 17:32:26 +0700

    Update absen-start.js

--- plugins/absen-start.js
+++ plugins/absen-start.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, usedPrefix, text }) => {
     conn.absen = conn.absen ? conn.absen : {}
     let id = m.chat
     if (id in conn.absen) {
-        await conn.sendButton(m.chat, `Masih ada absen di chat ini!\n\nketik *${usedPrefix}hapusabsen* untuk menghapus absen`.trim(), '© stikerin', 'HAPUS', `${usedPrefix}hapusabsen`)
+        await conn.sendButton(m.chat, `Masih ada absen di chat ini!\n\nketik *${usedPrefix}hapusabsen* untuk menghapus absen`.trim(), '© Haruno', 'HAPUS', `${usedPrefix}hapusabsen`)
         throw false
     }
     conn.absen[id] = [
-        await conn.sendButton(m.chat, `Absen dimulai`.trim(), '© stikerin', 'ABSEN', `${usedPrefix}absen`),
+        await conn.sendButton(m.chat, `Absen dimulai`.trim(), '© Haruno', 'ABSEN', `${usedPrefix}absen`),
         [],
         text
     ]
