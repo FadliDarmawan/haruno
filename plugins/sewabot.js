8cddc28888ea0eaf56f076c68a5f9407f0599e2b
FadliDarmawan <79369935+FadliDarmawan@users.noreply.github.com>
2021-08-20 17:54:04 +0700

    sewa.js

---
+++ plugins/sewabot.js
@@ -0,0 +1,43 @@
+let fetch = require('node-fetch')
+let { MessageType } = require('@adiwajshing/baileys')
+let handler = async(m, { conn }) => {
+    let kamisato = `┌ 「 Sewa 」
+    │ ✅Dapatkan fitur khusus premium.
+    │ ✅Masukkan bot ke group anda.
+    │
+    │ © Haruno
+    └────
+    Pembayaran:
+    Saweria: https://saweria.co/FadliStudio (+5.000)
+    Pulsa (XL): 6281943265086 (+5.000)
+    GoPay: 628112958665
+    Dana: 628112958665
+    OVO: 628112958665
+    
+    「Syarat dan Ketentuan」
+    1. Bot akan keluar jika sudah waktu nya keluar
+    2. Jangan di kick
+    3. Mematuhi syarat dan ketentuan bot pada #rules
+    4. Di larang mengirimkan sesuatu yang mengganggu, contoh: virtex, berbagai macam bug.
+    5. Di larang spam dalam penggunaan
+    `.trim()
+    const button = {
+        buttonText: 'List Harga',
+        description: kamisato,
+        sections:  [{title: "Silahkan di pilih", rows: [
+        {title: 'Premium', description: "Rp20.000\nDapatkan fitur premium.", rowId:".premium"},
+        {title: 'Permanen', description: "Rp15.000\nSewa bot tanpa batasan waktu,", rowId:".permanen"},
+        {title: '1 Bulan', description: "Rp10.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
+        {title: '1 Minggu', description: "Rp7.000\nSewa bot selama 1 minggu.", rowId:".masuk"},
+        {title: '1 Bulan', description: "GRATIS\nBot gratis 1 Hari.", rowId:".masuk"},
+        {title: 'OWNER', description: "Chat owner nya.", rowId:".maker"},
+       ] }],
+        listType: 1
+       }
+    conn.sendMessage(m.chat, button, MessageType.listMessage)
+}
+handler.tags = ['main']
+handler.command = /^(sewa)$/i
+handler.help = ['sewa']
+module.exports = handler
+//Haruno Bot
