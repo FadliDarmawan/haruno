eda5f8b100ae813b70adec4f28aa1adf32a7ea54
FadliDarmawan <79369935+FadliDarmawan@users.noreply.github.com>
2021-08-31 16:41:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -38,6 +38,7 @@ global.packname = 'Haruno'
 global.author = 'Fadli'
 
 global.wait = '「 ⏱️ 」Harap tunggu...'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 
