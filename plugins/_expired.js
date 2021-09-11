59b57272205b21a22f76b18948132cb2922c6e17
FadliDarmawan <79369935+FadliDarmawan@users.noreply.github.com>
2021-09-11 10:31:35 +0700

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -4,7 +4,7 @@ handler.before = async function (m) {
 
     if (m.isGroup && db.data.chats[m.chat].expired != 0) {
         if (new Date() * 1 >= db.data.chats[m.chat].expired) {
-            this.sendFile(m.chat, fs.readFileSync('./src/ex.mp4'), '', `waktu *${this.user.name}* di grup sudah habis, Sayonara`, 0, { thumbnail: Buffer.alloc(0) }).then(() => {
+            this.sendFile(m.chat, fs.readFileSync('./src/ex.mp4'), '', `waktu *${this.user.name}* di grup sudah habis, Sayonara`, m, 0, { thumbnail: Buffer.alloc(0) }).then(() => {
                 this.sendContact(m.chat, owner[0], this.getName(owner[0] + '@s.whatsapp.net')).then(() => {
                     this.groupLeave(m.chat).then(() => {
                         db.data.chats[m.chat].expired = 0
