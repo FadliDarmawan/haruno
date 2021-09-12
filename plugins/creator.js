0bc35f698abdde247ce00479b61392a40dbe63c6
FadliDarmawan <79369935+FadliDarmawan@users.noreply.github.com>
2021-09-12 13:27:40 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,7 @@
 function handler(m) {
+  m.reply('Chat jika penting, harap tidak spam.')
   this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
+  this.sendContact(m.chat, '6282215569001@s.whatsapp.net', 'Zaki', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
