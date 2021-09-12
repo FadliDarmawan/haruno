125479a13f1543bb853d6dc2598d5e84ed70e4cc
FadliDarmawan <79369935+FadliDarmawan@users.noreply.github.com>
2021-09-12 08:55:47 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,6 @@
 function handler(m) {
   this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
+  this.sendContact(m.chat, '6282215569001', 'Zaki', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
