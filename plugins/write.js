cbe33271e6ccb97715c78fede85f7cee7836aa59
FadliDarmawan <79369935+FadliDarmawan@users.noreply.github.com>
2021-09-12 14:36:43 +0700

    Update write.js

--- plugins/write.js
+++ plugins/write.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, args, usedPrefix, command }) => {
     let img = 'https://telegra.ph/file/b29dffd1931e4c521c8be.jpg'
     if (!args[0]) conn.sendFile(m.chat, 'https://telegra.ph/file/b29dffd1931e4c521c8be.jpg', '', `Harap masukkan tujuan dan nama file\nContoh: ${ usedPrefix + command } plugins/join.js`, m)
     require('fs').writeFileSync(`./${args[0]}`, m.quoted.text)
-    conn.sendButton(m.chat, await(await fetch(img)).buffer(), `Berhasil menyimpan sebagai ${args[0]}`, watermark, 'MENU', '.?', m)
+    conn.sendButtonImg(m.chat, await(await fetch(img)).buffer(), `Berhasil menyimpan sebagai ${args[0]}`, watermark, 'MENU', '.?', m)
 }
 handler.command = /^write$/i
 handler.owner = true
