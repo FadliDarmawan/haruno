0c60d317e9ac03308e87c330cc35cec8e1e5ad0e
FadliDarmawan <79369935+FadliDarmawan@users.noreply.github.com>
2021-09-26 15:15:41 +0700

    hmm

--- config.js
+++ config.js
@@ -38,9 +38,10 @@ global.packname = 'Haruno'
 global.author = 'Fadli'
 
 global.wait = '「 ⏱️ 」Harap tunggu...'
-global.eror = '*Server Error*'
+global.eror = '「❗」'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© Haruno'
+global.watermark = '© Haruno' //change the watermark (beberapa harus manual)
+global.image = 'https://telegra.ph/file/1893a59e90a39b1f593c7.jpg' //change the image
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 
