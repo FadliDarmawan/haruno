a5fd18e001af6b5f02e1f4bbe8e3f85570c6fe33
FadliDarmawan <79369935+FadliDarmawan@users.noreply.github.com>
2021-11-12 21:53:23 +0700

    2.2.0

--- plugins/stickernobg.js
+++ plugins/stickernobg.js
@@ -1,5 +1,6 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
+const axios = require('axios')
 const WSF = require('wa-sticker-formatter')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   let stiker = false
@@ -10,7 +11,12 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (/webp/.test(mime)) {
       let img = await q.download()
       if (!img) throw `balas stiker dengan perintah ${usedPrefix + command}`
-      wsf = new WSF.Sticker(img, {
+      let imgbase64 = img.toString('base64')
+      let data = await axios.post('https://salisganteng.pythonanywhere.com/api/remove-bg', {
+          'api-key': 'salisheker',
+          'image': imgbase64,
+      })
+      wsf = new WSF.Sticker(data.data.image, {
         pack: global.packname,
         author: global.author,
         crop: false,
@@ -18,19 +24,15 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     } else if (/image/.test(mime)) {
       let img = await q.download()
       if (!img) throw `balas gambar dengan perintah ${usedPrefix + command}`
-      wsf = new WSF.Sticker(img, {
+      let imgbase64 = img.toString('base64')
+      let data = await axios.post('https://salisganteng.pythonanywhere.com/api/remove-bg', {
+          'api-key': 'salisheker',
+          'image': imgbase64,
+      })
+      wsf = new WSF.Sticker(data.data.image, {
         pack: global.packname,
         author: global.author,
         crop: false,
-      })
-    } else if (/video/.test(mime)) {
-      if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
-      let img = await q.download()
-      if (!img) throw `balas video dengan perintah ${usedPrefix + command}`
-      wsf = new WSF.Sticker(img, {
-        pack: global.packname,
-        author: global.author,
-        crop: true,
       })
     } else if (args[0]) {
       if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
@@ -54,9 +56,9 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     // else throw `Gagal${m.isGroup ? ', balas gambarnya!' : ''}`
   }
 }
-handler.help = ['stiker ', 'stiker <url>']
+handler.help = ['stickernobg', 'stickernobg <url>']
 handler.tags = ['sticker']
-handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
+handler.command = /^s(tic?ker)?(wm)?nobg$/i
 
 module.exports = handler
 
--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,21 +1,12 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
-  if (!args[0].match(/tiktok/gi)) throw `url salah`
-
-  let res = await fetch(API('hardianto', '/api/download/tiktok', { url: args[0] }, 'apikey'))
-  if (!res.ok) throw eror
-  let json = await res.json()
-  // if (!json.status) throw json        
-  await m.reply(wait)
-  await conn.sendFile(m.chat, json.nowm, '', `${json.caption}\n\n© Haruno`, m)
-
+let handler = async(m, { conn, usedPrefix, args, command }) => {
+    if(!args[0]) throw `Uhm... URL nya mana?\n\nMasukkan link tiktok yang valid.\nContoh: ${usedPrefix + command} https://vt.tiktok.com/ZSwWCk5o/`
+    let res = await fetch(global.API('lolhum', '/api/tiktok', { url: args[0]}, 'apikey'))
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    await conn.sendFile(m.chat, json.result.link, 'video.mp4', watermark, m)
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.command = /^tiktok$/i
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
-
-handler.limit = true
-
+handler.help = ['tiktok <url>']
 module.exports = handler
