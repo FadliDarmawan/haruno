559a8137f1fb167b6f632f957cb4e872a3bd4019
FadliDarmawan <fadli.darmawan03@gmail.com>
2021-12-29 12:22:10 +0700

    delete file g penting, ganti firstchat

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -9,7 +9,7 @@ handler.all = async function (m) {
     if (m.isGroup) return
     if (db.data.settings.groupOnly) return
     let user = global.db.data.users[m.sender]
-    let name = conn.user.name
+    let name = conn.getName(m.sender)
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
     await conn.send2ButtonLoc(m.chat, await(await fetch(thumbfoto)).buffer(), `
 *Hi ${name}, ${ucapan()}*
