f701df10b51b2b5eb49405fb81253f7ed07e282b
FadliDarmawan <fadli.darmawan03@gmail.com>
2022-01-12 13:36:26 +0700

    2.4.0 (lanjutan)

--- plugins/harunoff.js
+++ plugins/harunoff.js
@@ -1,12 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
-    let gc1 = '628112958665-1625393837@g.us'
-    let gc2 = '628112958665-1628163967@g.us'
-    let gc3 = '628112958665-1571053173@g.us'
-    let _gc1 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc1)
-    let _gc2 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc2)
-    let _gc3 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc3)
-    let caption = `
+    try {
+        let gc1 = '628112958665-1625393837@g.us'
+        let gc2 = '628112958665-1628163967@g.us'
+        let gc3 = '628112958665-1571053173@g.us'
+        let _gc1 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc1)
+        let _gc2 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc2)
+        let _gc3 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc3)
+        let caption = `
 *Join juga group official Haruno Bot!*
 Sekedar meramaikan, atau juga jika ada informasi mengenai bot.
 
@@ -21,7 +22,27 @@ ${_gc3}
 
 Harap patuhi rules dan peraturan pada masing masing group ya! Terimakasih.
 `.trim()
-    await conn.send2ButtonLoc(m.chat, await(await fetch(thumbfoto)).buffer(), caption, watermark, 'Owner', '.owner', 'Sewa', '.sewa', m, { contextInfo: { mentionedJid: m.sender }})
+        await conn.send2ButtonLoc(m.chat, await(await fetch(thumbfoto)).buffer(), caption, watermark, 'Owner', '.owner', 'Sewa', '.sewa', m, { contextInfo: { mentionedJid: m.sender }})
+    } catch (e) {
+        let res = await fetch('https://raw.githubusercontent.com/FadliDarmawan/haruno-server/main/files/src.json')
+        let json = await res.json()
+        let tulisan = `
+*Join juga group official Haruno Bot!*
+Sekedar meramaikan, atau juga jika ada informasi mengenai bot.
+
+*Group 1*
+${json.links[0]}
+
+*Group 2*
+${json.links[1]}
+
+*Group 3*
+${json.links[2]}
+
+Harap patuhi rules dan peraturan pada masing masing group ya! Terimakasih.
+`.trim()
+await conn.send2ButtonLoc(m.chat, await(await fetch(thumbfoto)).buffer(), tulisan, watermark, 'Owner', '.owner', 'Sewa', '.sewa', m, { contextInfo: { mentionedJid: m.sender }})
+    }
 }
 handler.help = ['groupofficial']
 handler.tags = ['main']
