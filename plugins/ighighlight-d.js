b52ee97b9fd1689d1617689d4a27a639d507ce6c
FadliDarmawan <fadli.darmawan03@gmail.com>
2022-01-13 20:44:52 +0700

    hmm

--- plugins/ighighlight-d.js
+++ plugins/ighighlight-d.js
@@ -3,8 +3,7 @@ let handler = async(m, { conn, args }) => {
     let res = await fetch(global.API('neoxr', '/api/igh', {q: args[0], id: args[1]}, 'apikey'))
     let hako = await res.json()
     for (const { url } of hako.data)
-    let img = await(await fetch(url)).buffer() 
-    await conn.sendFile(m.chat, img,  'url',  watermark, m)
+    await conn.sendFile(m.chat, url,  'url',  watermark, m)
 }
 handler.command = /^highlight$/i
 module.exports = handler
