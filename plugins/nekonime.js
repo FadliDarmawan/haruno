7f1d77585ec5532aed00f0df6337499643d22a79
Fadli Darmawan <fadli.darmawan03@gmail.com>
2022-04-04 17:46:13 +0700

    2.5.0 Update

--- plugins/nekonime.js
+++ plugins/nekonime.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
     let res = 'https://nekos.life/api/neko'
-    if(!res.ok) await conn.sendButtonImg(m.chat, await(await fetch(image)).buffer(), 'Sorry for the error. Report it?', watermark, 'Report', '.report cuddle.js', m)
-    await conn.sendFile(m.chat, res.url, '', watermark, m)
+    if(!res.ok) await conn.sendButtonImg(m.chat, await(await fetch(internal)).buffer(), 'Sorry for the error. Report it?', watermark, 'Report', '.report cuddle.js', m)
+    await conn.sendFile(m.chat, res.neko, '', watermark, m)
 }
 handler.command = /^nekonime$/i
 handler.tags = ['anime']
