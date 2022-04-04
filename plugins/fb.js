29a782207ddafe249499206073fab0ecfcea31bb
Fadli Darmawan <fadli.darmawan03@gmail.com>
2022-04-04 16:59:30 +0700

    2.5.0 Update

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,10 +1,8 @@
 const xa = require('xfarr-api')
 let handler = async(m, { conn, usedPrefix, args, command }) => {
   if (!args[0]) throw `Masukkan URL Facebook yang ingin di download!\n\nContoh: ${usedPrefix + command} https://www.facebook.com/100009307660961/videos/2850837675236460/`
-  xa.facebook(args[0])
-    .then(data => {
-      conn.sendFile(m.chat, data.medias[1].url, '', watermark, m)
-    })
+  let result = await xa.facebook(args[0])
+  conn.sendFile(m.chat, result.medias[1].url, '', watermark, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
